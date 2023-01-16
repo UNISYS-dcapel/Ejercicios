9ce0ddc6ca2280952abd5520993bd6d163a82ed9
UNISYS-dcapel <UNISYS.dcapel@iberia.es>
2023-01-16 18:11:09 +0100

    Ejercicio01

---
+++ src/main/java/com/unisys/ejercicios/Ejercicio01.java
@@ -0,0 +1,14 @@
+package com.unisys.ejercicios;
+
+public class Ejercicio01 {
+    public static void main(String[] args){
+        int numero = 10;
+        short marianico = 12;
+        long gasol = 18;
+        boolean independencia = false;
+        double presupuesto = 12.5;
+        float longitud = 12.68F;
+        var suma = gasol + marianico;
+        System.out.println(suma);
+    }
+}
